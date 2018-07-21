8b972b8e50640c27109cffc7db18f34af5c6c522
IEUser <IEUser@IE11Win7>
2018-07-21 14:29:17 +0900

    maven-surefire-plugin WARNING 해결
    main에서 빌드하면 classes 경로로 들어감
    C:\Users\IEUser\git\nextit.common\target\classes\nextit\common\MemberVO.class
    test에서 빌드하면 test-classes 경로로 들어감
    C:\Users\IEUser\git\nextit.common\target\test-classes\nextit\common\MemberTest.class

---
+++ src/main/java/nextit/common/MemberVO.java
@@ -0,0 +1,7 @@
+package nextit.common;
+
+public class MemberVO {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
---
+++ src/test/java/nextit/common/MemberTest.java
@@ -0,0 +1,9 @@
+package nextit.common;
+
+public class MemberTest {
+
+	public static void main(String[] args) {
+		System.out.println("TEST2");
+	}
+
+}
